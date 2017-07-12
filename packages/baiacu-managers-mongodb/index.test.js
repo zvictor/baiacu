243d998fa7a8884a70a22a093a3f40c8fad20d85
zVictor <victor0110@msn.com>
2017-07-12 16:04:34 +0200

    initial commit of baiacu-managers-mongodb

---
+++ packages/baiacu-managers-mongodb/index.test.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import { MongoClient } from 'mongodb';
+import Manager from './index';
+
+let connection;
+const collection = 'baiacu-managers-mongodb'
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// Hooks                                                                                          //
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+test.before('mongodb should connect', async t => {
+  connection = await MongoClient.connect('mongodb://localhost:27017/tests');
+});
+
+test.after('mongodb connection should be closed', async t => {
+  await connection.close();
+});
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// Tests                                                                                          //
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+test('manager should select right collection', async t => {
+  const manager = new Manager({connection, collection});
+
+  t.is(manager.collection.collectionName, collection);
+});
+
+test.serial('manager should be able to find content', async t => {
+  const manager = new Manager({connection, collection});
+
+  t.is(await manager.find(), []);
+});
